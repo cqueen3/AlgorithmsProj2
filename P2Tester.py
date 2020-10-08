9b37d05d92c021ac6b8247877ddb0587ad4133ff
Corey Queen <58435870+cqueen3@users.noreply.github.com>
2020-10-08 10:34:49 -0500

    Add files via upload

--- P2Tester.py
+++ P2Tester.py
@@ -1,11 +1,29 @@
 import random
 
-forwardsList = []
-backwardsList = []
-randList = []
+forwardsList1 = []
+backwardsList1 = []
+randList1 = []
+
+for x in range(1000):
+    forwardsList1.append(x)
+    backwardsList1.append(10000-x)
+    randList1.append(random.randint(1, 50000))
+    
+forwardsList2 = []
+backwardsList2 = []
+randList2 = []
+
 
 for x in range(10000):
-    forwardsList.append(x)
-    backwardsList.append(10000-x)
-    randList.append(random.randint(1, 50000))
-    
+    forwardsList2.append(x)
+    backwardsList2.append(10000-x)
+    randList2.append(random.randint(1, 50000))
+
+forwardsList3 = []
+backwardsList3 = []
+randList3 = []
+
+for x in range(100000):
+    forwardsList3.append(x)
+    backwardsList3.append(10000-x)
+    randList3.append(random.randint(1, 50000))

f0ddf3b6375758855a74f341c80299156c5d55cc
Corey Queen <58435870+cqueen3@users.noreply.github.com>
2020-10-08 10:30:58 -0500

    Add files via upload

--- P2Tester.py
+++ P2Tester.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import random
+
+forwardsList = []
+backwardsList = []
+randList = []
+
+for x in range(10000):
+    forwardsList.append(x)
+    backwardsList.append(10000-x)
+    randList.append(random.randint(1, 50000))
+    
